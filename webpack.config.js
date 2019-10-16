58c99ec687e5e75eb38c80abf623adca2367446f
Andrey Ivanov <andrey-tm203@mail.ru>
2019-10-16 20:59:27 +0300

    configure project

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: path.join(__dirname, `src/index.js`),
+  output: {
+    path: path.join(__dirname, `public`),
+    filename: `bundle.js`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    port: 1337,
+    compress: false,
+    open: true,
+  },
+  devtool: `source-map`
+};
