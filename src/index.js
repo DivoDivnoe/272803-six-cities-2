cf27c37ec56287ef73f71d131c84f2211e2c14f7
Andrey Ivanov <andrey-tm203@mail.ru>
2019-10-19 16:43:55 +0300

    add propTypes, update MainPage component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainPage from './components/main-page/main-page.jsx';
 
+const descriptions = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+];
+
 const init = () => {
-  ReactDOM.render(<MainPage />, document.querySelector(`#root`));
+  ReactDOM.render(<MainPage descriptions={descriptions} />, document.querySelector(`#root`));
 };
 
 init();
